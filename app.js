0376ee0ec45d6eb21b4be9ea835bbd1a9846beb4
“Minh <minhtt21@fe.edu.vn>
2021-08-15 17:28:21 +0700

    ssl modified

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const pg_conn = new Pool (
         database: 'd246b1gt36nk36',
         password: '2eec60707f51d97aa019f41e3b6eb42b19b4395be9b2811cac00294f8b137c13',
         port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+          },
     });
 pg_conn.query("SELECT * FROM product", (error, results) => 
     {
